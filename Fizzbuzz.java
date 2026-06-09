a8b37e8b8698c6a7ff79e610133da0b93b71c95e
NRG SANTA <nrgsanta1@gmail.com>
2026-06-09 10:29:08 -0600

    Add files via upload

---
+++ Fizzbuzz.java
@@ -0,0 +1,18 @@
+public class Fizzbuzz {
+
+    public static void main(String[] args) {
+        
+        for (int i = 1; i <= 100; i++) {
+                 
+            if(i % 3 == 0 && i % 5 == 0) {
+                System.out.println("Fizzbuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
