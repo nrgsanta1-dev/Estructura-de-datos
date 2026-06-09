55463ebf067acf8d676245761a3a8ab6ac46f204
NRG SANTA <nrgsanta1@gmail.com>
2026-06-09 10:10:40 -0600

    Add files via upload
    Reto1

---
+++ reto1.java
@@ -0,0 +1,21 @@
+public class reto1 {
+    /*
+ URL oficial de Java:
+ https://www.java.com/
+ */
+
+    // Comentario de una sola línea
+
+    /*
+     Comentario
+     de varias líneas
+     */
+
+    /**
+     * Comentario de documentación (JavaDoc)
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola Java");
+    }
+
+}
